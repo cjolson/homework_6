d9f995f76a4bf9e7199b8fb55439bddb9d151143
Clara Olson <cjolson@andrew.cmu.edu>
2018-10-15 23:23:06 -0400

    Deleted a lot of JS from the weekend. Using better code practices. Contained class definitions in their own file for organization. Buttons are interactive, but currently they don't store any info.

---
+++ Scripts/Cart.js
@@ -0,0 +1,9 @@
+/**class Cart {
+  constructor() {
+    this.subtotal = 0;
+    this.total = 0;
+    this.shipping = 0;
+    this.numOfItemsInCart = 0;
+    this.items = {};
+  }
+}**/
---
+++ Scripts/main.js
@@ -0,0 +1,124 @@
+var ids = ['dog-harness', 'cat-harness', 'gps-tracker', 
+            'food-attachment', 'water-attachment', 'cat-backpack']
+var names = ['Cat Harness', 'Dog Harness', 'GPS Tracker', 
+              'Food Attachment', 'Water Attachment', 'Cat Backpack'];
+var images = [['Assets/Items/Dog-Harness/dog-harness-1.jpg'], 
+              ['Assets/Items/Dog-Harness/dog-harness-1.jpg'],
+              ['Assets/Items/Dog-Harness/dog-harness-1.jpg'], 
+              ['Assets/Items/Dog-Harness/dog-harness-1.jpg'], 
+              ['Assets/Items/Dog-Harness/dog-harness-1.jpg'],
+              ['Assets/Items/Dog-Harness/dog-harness-1.jpg']];
+var prices = ['$24.95', '$39.95', '$54.95', '$19.95', '$17.95', '$64.95'];
+var ratings = [5, 5, 5, 5, 5, 5];
+var reviews = [[], [], [], [], [], []];
+var numOfReviews = [88, 139, 102, 32, 35, 19];
+var tags = [new Set(['cats', 'harnesses', 'featured']),
+             new Set(['dogs', 'harnesses', 'featured']),
+             new Set(['cats', 'dogs', 'accessories', 'featured']),
+             new Set(['cats', 'dogs', 'accessories']),
+             new Set(['cats', 'dogs', 'accessories']),
+             new Set(['cats', 'dogs', 'backpacks'])];
+
+var num_of_items = 6;
+var all_items;
+var tag_status = {'cats': false,
+                  'dogs': false,
+                  'harnesses': false,
+                  'accessories': false,
+                  'backpacks': false}
+
+/**
+  * main(): runs when the window loads
+  */
+$(window).on('load', function() {
+  console.log(document.URL);
+  all_items = makeItemList(); // sets value of global variable full_item_list
+  populateItems('#shop', all_items);
+  populateItems('#featured', getFilteredList("featured", all_items));
+});
+
+/**
+  * Returns a list contiaining Item Objects.
+  */
+  makeItemList = function() {
+    var dict = {};
+    for(var i=0; i<num_of_items; i++) {
+      console.log(ids[i]);
+      var key = ids[i];
+      var item = new Item(key, names[i], images[i], prices[i], ratings[i], 
+                          reviews[i], numOfReviews[i], tags[i]);
+      dict[key] = item;
+      console.log('Created Item: '+item.getName());
+    }
+    return dict;
+  };
+
+/**
+  * Populate webpages with visible item objects.
+  */
+  populateItems = function(container, items_dict) {
+    var html_string = "";
+    for (var i=0; i<Object.keys(items_dict).length; i++) {
+      var key = ids[i];
+      var item = items_dict[key];
+      html_string += item.getDiv();
+      console.log('Populate '+container+' with '+item.getName());
+    }
+    $(container).html(html_string);
+  };
+
+/**
+  * Returns an item list containing a tag.
+  */
+  getFilteredList = function(tag, items_dict) {
+    var new_dict = [];
+    for (var i=0; i<Object.keys(items_dict).length; i++) {
+      var key = ids[i];
+      var item = items_dict[key];
+      if (item.hasTag(tag)) {
+        new_dict[key] = item;
+        console.log('Filtered by '+tag+': '+item.getName());
+      }  
+    }
+    return new_dict;
+  };
+
+/**
+  *
+  */
+  selectColor = function(color_button) {
+    $('.select-color').each( function () {
+      makeSelection(color_button, $(this));
+    })
+  };
+
+/**
+  *
+  */
+  selectSize = function(size_button) {
+    $('.select-size').each( function () {
+      makeSelection(size_button, $(this));
+    })
+  };
+
+/**
+  *
+  */
+  selectImage = function(thumbnail) {
+    $('.thumbnail-image').each( function () {
+      makeSelection(thumbnail, $(this));
+    })
+  };
+
+/**
+  *
+  */
+  makeSelection = function (button, choice) {
+    console.log(button.getAttribute('id'));
+    if (choice.is('#'+button.getAttribute('id'))) {
+      choice.addClass('selected');
+      choice.removeClass('not-selected');
+    } else {
+      choice.addClass('not-selected');
+    }
+  };
