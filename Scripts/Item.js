7a4899ad2c4d43563b8dbe61f7d02a52a31cbd30
Clara Olson <cjolson@andrew.cmu.edu>
2018-10-26 00:27:36 -0400

    Added quantity and fixed filtering functions.

--- Scripts/Item.js
+++ Scripts/Item.js
@@ -1,26 +1,26 @@
 function Item(id, name, allImages, price, rating, reviews, numOfREviews, tags) {
-  
+
   this.id = id;                     // id
-  this.name = name;                 // string 
+  this.name = name;                 // string
   this.link = 'dog-harness.html' //id+'.html';           // url to item page
   this.allImages = allImages;       // array of all image src (shop = 0)
   this.price = price;               // string
   this.rating = rating;             // new Rating(rating); // Rating object
   this.review = reviews;            // array of Review objects
   this.numOfRevies = numOfReviews;  // total number of reviews
-  this.tags = tags;        // keywords for filtering and picking recommend items
+  this.tags = tags;        // set of keywords for filtering and picking recommend items
   this.color = null;
   this.size = null;
-  this.details = `<p>Lorem ipsum dolor sit amet, risus nec elit, amet tellus 
-                    volutpat, convallis in pede. Erat amet et, vel proin, 
-                    parturient ultricies pede. Sodales ridiculus cum, dui non 
+  this.details = `<p>Lorem ipsum dolor sit amet, risus nec elit, amet tellus
+                    volutpat, convallis in pede. Erat amet et, vel proin,
+                    parturient ultricies pede. Sodales ridiculus cum, dui non
                     tellus.</p>
-                  <p>Velit mauris, magna amet, morbi odio non. Mauris dolor 
+                  <p>Velit mauris, magna amet, morbi odio non. Mauris dolor
                     egestas, nulla varius ante, eget nunc non.</p>`;
   this.div = `<div class="shop-item">
                 <a href="`+this.link+`">
                   <div class="shop-item-preview">
-                    <img class="item-browing-img" src="`+this.allImages[0]+`" 
+                    <img class="item-browing-img" src="`+this.allImages[0]+`"
                       alt="`+this.name+`">
                   </div>
                   <div class="color-selection-preview">
@@ -35,16 +35,29 @@ function Item(id, name, allImages, price, rating, reviews, numOfREviews, tags) {
                 </div>`;
 };
 
+Item.prototype.getTags = function() {
+  return this.tags;
+};
+
 Item.prototype.getDiv = function() {
   return this.div;
-}
+};
 
 Item.prototype.getName = function() {
   return this.name;
-}
+};
 
-Item.prototype.hasTag = function(other_tag) {
-  return this.tags.has(other_tag);
+Item.prototype.hasTag = function(other_tags) {
+  var bool = false;
+  other_tags.forEach(tag => {
+    console.log('checking '+tag)
+    if(this.tags.has(tag)){
+      console.log(true);
+      bool = true;
+    }
+  });
+  console.log(false);
+  return bool;
 }
 
 Item.prototype.setColor = function(color) {
@@ -69,11 +82,11 @@ Item.prototype.getSize = function() {
     this.name = name || "";
     this.shopImg = shopImg || "";
     this.price = price || "";
-    this.details = `<p>Lorem ipsum dolor sit amet, risus nec elit, amet tellus 
-                      volutpat, convallis in pede. Erat amet et, vel proin, 
-                      parturient ultricies pede. Sodales ridiculus cum, dui non 
+    this.details = `<p>Lorem ipsum dolor sit amet, risus nec elit, amet tellus
+                      volutpat, convallis in pede. Erat amet et, vel proin,
+                      parturient ultricies pede. Sodales ridiculus cum, dui non
                       tellus.</p>
-                    <p>Velit mauris, magna amet, morbi odio non. Mauris dolor 
+                    <p>Velit mauris, magna amet, morbi odio non. Mauris dolor
                       egestas, nulla varius ante, eget nunc non.</p>`;
     this.rating = rating || "";
     this.numOfRevies = numOfReviews || "";
@@ -83,7 +96,7 @@ Item.prototype.getSize = function() {
     this.size = null;
     this.html = `<a href="`+this.link+`">
                   <div class="shop-item-preview">
-                    <img class="item-browing-img" src="`+this.shopImg+`" 
+                    <img class="item-browing-img" src="`+this.shopImg+`"
                       alt="`+this.name+`">
                   </div>
                   <div class="color-selection-preview">
@@ -96,11 +109,11 @@ Item.prototype.getSize = function() {
                   <div class="item-price-browsing">`+this.price+`</div>
                 </a>`;
   };
-  
+
   tagPresent(tag) {
     return this.tags.indexOf(tag) >= 0;
   };
-  
+
   getDiv() {
     var div = document.createElement("div");
     div.className = "shop-item";
--- Scripts/main.js
+++ Scripts/main.js
@@ -1,22 +1,22 @@
-var ids = ['dog-harness', 'cat-harness', 'gps-tracker', 
+var ids = ['dog-harness', 'cat-harness', 'gps-tracker',
             'food-attachment', 'water-attachment', 'cat-backpack']
-var names = ['Cat Harness', 'Dog Harness', 'GPS Tracker', 
+var names = ['Cat Harness', 'Dog Harness', 'GPS Tracker',
               'Food Attachment', 'Water Attachment', 'Cat Backpack'];
-var images = [['Assets/Items/Dog-Harness/dog-harness-1.jpg'], 
+var images = [['Assets/Items/Dog-Harness/dog-harness-1.jpg'],
               ['Assets/Items/Dog-Harness/dog-harness-1.jpg'],
-              ['Assets/Items/Dog-Harness/dog-harness-1.jpg'], 
-              ['Assets/Items/Dog-Harness/dog-harness-1.jpg'], 
+              ['Assets/Items/Dog-Harness/dog-harness-1.jpg'],
+              ['Assets/Items/Dog-Harness/dog-harness-1.jpg'],
               ['Assets/Items/Dog-Harness/dog-harness-1.jpg'],
               ['Assets/Items/Dog-Harness/dog-harness-1.jpg']];
 var prices = ['$24.95', '$39.95', '$54.95', '$19.95', '$17.95', '$64.95'];
 var ratings = [5, 5, 5, 5, 5, 5];
 var reviews = [[], [], [], [], [], []];
 var numOfReviews = [88, 139, 102, 32, 35, 19];
-var tags = [new Set(['cats', 'harnesses', 'featured']),
+var tags = [new Set(['cats', 'harnesses', 'featured', 'recommended']),
              new Set(['dogs', 'harnesses', 'featured']),
-             new Set(['cats', 'dogs', 'accessories', 'featured']),
-             new Set(['cats', 'dogs', 'accessories']),
-             new Set(['cats', 'dogs', 'accessories']),
+             new Set(['cats', 'dogs', 'accessories', 'featured', 'recommended']),
+             new Set(['cats', 'dogs', 'accessories', 'recommended']),
+             new Set(['cats', 'dogs', 'accessories', 'recommended']),
              new Set(['cats', 'dogs', 'backpacks'])];
 
 var num_of_items = 6;
@@ -34,7 +34,8 @@ $(window).on('load', function() {
   console.log(document.URL);
   all_items = makeItemList(); // sets value of global variable full_item_list
   populateItems('#shop', all_items);
-  populateItems('#featured', getFilteredList("featured", all_items));
+  populateItems('#featured', getFilteredList(["featured"], all_items));
+  populateItems('#recommended', getFilteredList(["recommended"], all_items));
 });
 
 /**
@@ -45,7 +46,7 @@ $(window).on('load', function() {
     for(var i=0; i<num_of_items; i++) {
       console.log(ids[i]);
       var key = ids[i];
-      var item = new Item(key, names[i], images[i], prices[i], ratings[i], 
+      var item = new Item(key, names[i], images[i], prices[i], ratings[i],
                           reviews[i], numOfReviews[i], tags[i]);
       dict[key] = item;
       console.log('Created Item: '+item.getName());
@@ -58,11 +59,14 @@ $(window).on('load', function() {
   */
   populateItems = function(container, items_dict) {
     var html_string = "";
+    console.log(items_dict);
     for (var i=0; i<Object.keys(items_dict).length; i++) {
       var key = ids[i];
       var item = items_dict[key];
-      html_string += item.getDiv();
-      console.log('Populate '+container+' with '+item.getName());
+      if (item) {
+        html_string += item.getDiv();
+        console.log('Populate '+container+' with '+item.getName());
+      }
     }
     $(container).html(html_string);
   };
@@ -70,16 +74,18 @@ $(window).on('load', function() {
 /**
   * Returns an item list containing a tag.
   */
-  getFilteredList = function(tag, items_dict) {
-    var new_dict = [];
+  getFilteredList = function(tags, items_dict) {
+    var new_dict = {};
     for (var i=0; i<Object.keys(items_dict).length; i++) {
       var key = ids[i];
       var item = items_dict[key];
-      if (item.hasTag(tag)) {
+      console.log(item);
+      if (item.hasTag(tags)) {
         new_dict[key] = item;
-        console.log('Filtered by '+tag+': '+item.getName());
-      }  
+        console.log('Filtered by '+tags+': '+item.getName());
+      }
     }
+    console.log(new_dict);
     return new_dict;
   };
 
@@ -89,9 +95,8 @@ $(window).on('load', function() {
   selectColor = function(color_button) {
     $('.select-color').each( function () {
       makeSelection(color_button, $(this));
+      console.log(color_button.value)
     })
-    console.log($(this));
-    //$('#'+color_button.getAttribute('id')).find('.selection-label').addClass('show-label');
   };
 
 /**
